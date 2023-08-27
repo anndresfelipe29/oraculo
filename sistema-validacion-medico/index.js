1975e489aa5218d75d541b850dbee6c894270fad
anndresfelipe29 <afgomezs@correo.udistrital.edu.co>
2023-08-27 16:50:39 -0500

    new endpoint to save users

--- sistema-validacion-medico/index.js
+++ sistema-validacion-medico/index.js
@@ -1,6 +1,7 @@
 
 //import contrato from "./oraculo.js"
 const express = require('express')
+const Joi = require('joi');
 const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring');
@@ -12,13 +13,14 @@ const port = 3001
 
 
 const usuarios = require('./usuarios')
+const Medico = require('./Medico')
 //  const ejemplo = require('./eventos')
 
 app.get('/buscar-usuario', (req, res) => {
   try {
     let identificacion = req.query.identificacion;
     
-    respuesta = usuarios.validarUsuario(parseInt(identificacion))
+    let respuesta = usuarios.validarUsuarioDeArchivo(parseInt(identificacion), "usuarios.json")
     if(respuesta == undefined){
       res.status(404).send({response: 'Not found'})
     }
@@ -36,25 +38,77 @@ app.get('/validar-usuario', (req, res) => {
     let identificacion = req.query.identificacion;
     let contrasena = req.query.contrasena;
     
-    respuesta = usuarios.validarUsuario(parseInt(identificacion))
+    let respuesta = usuarios.validarUsuarioDeArchivo(parseInt(identificacion), "usuarios.json")
     if(respuesta == undefined){
-      res.status(404).send({response: 'Not found'});
-      
+      res.status(404).json({response: 'Not found'});
+      return
     }
     if(respuesta.identificacion != contrasena) {
-      res.status(403).send({response: 'Fail data'});
-      
+      res.status(403).json({response: 'Fail data'});
+      return
     }
-    res.send(respuesta)
+    res.json(respuesta)
     res.status(200)    
 
   } catch(err) {
-    res.status(500).send({response: 'Internal server error'})
+    console.log(err)
+    res.status(500).json({response: 'Internal server error'})
   }
     
 })
 
+app.post('/crear-usuario', (req, res) => {
+  try {
+    const medicoSchema = Joi.object({
+      identificacion: Joi.string().required(),
+      nombre: Joi.string().required(),
+      apellido: Joi.string().required(),
+      especialidad: Joi.string().required(),
+      activo: Joi.boolean().required(),
+    });
+  
+    const { error } = medicoSchema.validate(req.body);
+    
+    if (error) {
+      return res.status(400).json({response: error.details[0].message});
+    }
+    const nuevoMedico = new Medico(
+      req.body.identificacion,
+      req.body.nombre,
+      req.body.apellido,
+      req.body.especialidad,
+      req.body.activo
+    );
+
+    usuarios.actualizacion(nuevoMedico, "usuarios.json")
+
+    res.status(201).json({ message: 'Medico creado correctamente' });
+
+  } catch(err) {
+    console.error(err)
+    res.status(err.code).send({response: err.msg})
+  }
+    
+})
+
+
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`)
-  usuarios.cargarUsuarios()
+  //usuarios.cargarUsuarios()
+  try {
+    usuarios.cargarUsuariosDeArchivo("usuarios.json")
+
+    usuarios.actualizacion({
+      identificacion: "1007",
+      nombre: "Pipe",
+      apellido: "Gómez",
+      especialidad: "Gastroenterologo",
+      activo: true
+  }, "usuarios.json")
+
+  } catch (error) {
+    console.log(error)
+  }
+  
+
 })
--- sistema-validacion-medico/usuarios.js
+++ sistema-validacion-medico/usuarios.js
@@ -1,49 +1,52 @@
+const { Console } = require('console');
 const Medico = require('./Medico')
+const fs = require('fs');
 const usuarios = new Map();
+
 
 exports.cargarUsuarios = () => {
     usuarios.set(1000, {
-        identificacion: "1000", 
+        identificacion: "1000",
         nombre: "Pipe",
         apellido: "Gómez",
         especialidad: "Gastroenterologo",
         activo: true
-     })
+    })
     usuarios.set(1001, {
-        identificacion: "1001", 
+        identificacion: "1001",
         nombre: "Pipe",
         apellido: "Gómez",
         especialidad: "Gastroenterologo",
         activo: true
-     })
+    })
     usuarios.set(1002, {
-        identificacion: "1002", 
+        identificacion: "1002",
         nombre: "Pipe",
         apellido: "Gómez",
         especialidad: "Gastroenterologo",
         activo: true
-     })
+    })
     usuarios.set(1003, {
-        identificacion: "1003", 
+        identificacion: "1003",
         nombre: "Pipe",
         apellido: "Gómez",
         especialidad: "Gastroenterologo",
         activo: true
-     })
+    })
     usuarios.set(1004, {
-        identificacion: "1004", 
+        identificacion: "1004",
         nombre: "Pipe",
         apellido: "Gómez",
         especialidad: "Gastroenterologo",
         activo: false
-     })
+    })
     usuarios.set(1005, {
-        identificacion: "1005", 
+        identificacion: "1005",
         nombre: "Pipe",
         apellido: "Gómez",
         especialidad: "Gastroenterologo",
         activo: true
-     })
+    })
 
 }
 
@@ -52,5 +55,64 @@ exports.validarUsuario = (identificacion) => {
     console.log(respuesta)
     //console.log(usuarios.size)
     return respuesta
-    
+
+}
+
+exports.validarUsuarioDeArchivo = (identificacion, nombreArchivo) => {
+    let usuarios = this.cargarUsuariosDeArchivo(nombreArchivo)
+    let respuesta = usuarios[identificacion]
+    console.log(respuesta)
+    //console.log(usuarios.size)
+    return respuesta
+
+}
+
+
+exports.cargarUsuariosDeArchivo = (nombreArchivo) => {
+    try {
+        let value = leerArchivoJSON(nombreArchivo)
+        console.log(value)
+        return value
+    } catch (error) {
+        console.error('Error al leer el archivo:', error);
+        throw { "code": 500, "msg": "Error al leer el archivo" }
+    }
+}
+
+async function escribirArchivoJSON(nombreArchivo, jsonData) {
+    try {
+        await fs.writeFileSync(nombreArchivo, JSON.stringify(jsonData, null, 2));
+        console.log('Archivo JSON actualizado correctamente.');
+    } catch (error) {
+        throw { "code": 500, "msg": "error" }
+    }
+}
+
+function leerArchivoJSON(nombreArchivo) {
+    try {
+        const data = fs.readFileSync(nombreArchivo, 'utf8');
+        const jsonData = JSON.parse(data);
+        return jsonData;
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.actualizacion = (nuevoValor, nombreArchivo) => {
+
+    const jsonData = leerArchivoJSON(nombreArchivo);
+
+    // Modificar el objeto JSON
+    console.log(nuevoValor.identificacion)
+
+    if (jsonData[nuevoValor.identificacion] != undefined) {
+        console.log("el valor ya existe")
+        throw { "code": 400, "msg": "El valor ya existe" }
+    } else {
+        jsonData[nuevoValor.identificacion] = nuevoValor;
+        console.log("el valor")
+        //console.log(jsonData)
+        escribirArchivoJSON(nombreArchivo, jsonData);
+    }
+
 }
